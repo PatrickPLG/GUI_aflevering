1db37c5fff34bfcb89b046371596c7d10e346879
PatrickPLG <44425816+PatrickPLG@users.noreply.github.com>
2020-02-21 08:50:07 +0100

    Updates + time add

---
+++ Opgaver/TidTest.py
@@ -0,0 +1,21 @@
+# En instans repræsenterer en dato.
+class Date:
+    # Konstruktør til at oprette en dato
+    def __init__(self, hour):
+        # Variable til repræsentation af dato
+        self.hour = hour
+        
+    # Ændrer datoen til næste dag
+    def setToNextDate(self):
+        self.hour = self.hour + 1
+        self.checkDayOverflow()
+
+    # Returnerer datoen som en streng
+    def toString(self):
+        return str(self.hour)
+
+    # Kontrollerer for specialtilfælde hvor day
+    # overskrider antal dage i måneden
+    def checkDayOverflow(self):
+        if (self.hour > 21):
+            self.hour = 1
