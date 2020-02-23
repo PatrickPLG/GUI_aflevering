35f1750e15662794e84f5e901b8af34cbc276abf
Nicklas K. Christensen <44356711+Klint02@users.noreply.github.com>
2020-02-23 20:30:11 +0100

    Major update

---
+++ Opgaver/frameUpdater.py
@@ -0,0 +1,15 @@
+import time
+
+class frames:
+    def dogFrameUpdater(self):
+        gameRunning = True
+        dogFrames = ["Assets/Dog graphics/Dog1.gif",
+                     "Assets/Dog graphics/Dog2.gif",
+                     "Assets/Dog graphics/Dog3.gif",
+                     "Assets/Dog graphics/Dog4.gif"]
+        Frames = len(dogFrames)
+        while gameRunning == True:
+            Frames = Frames + 1
+            time.sleep(1)
+            if (Frames == 5):
+                Frames = 0
